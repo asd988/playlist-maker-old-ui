8947c12d7a42514d3b2ec2a05055f8bb82d8b968
BuildTools <enderhd.98@gmail.com>
2022-11-04 19:23:25 +0100

    stuff

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 console.log("Hello world!")
+
+const stateKey = 'spotify_auth_state';
+const clientId = 'fd19eb51d7204c3f9096c4751e6c14fd';
+const redirectUri = "http://localhost:8888/callback"
+
+
+
+let generateRandomString = function (length) {
+    let text = '';
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+};
+
+function login() {
+    console.log("logging in")
+
+    const state = generateRandomString(16)
+    Cookies.set(stateKey, state)
+
+    const scope = "user-read-private user-read-email"
+
+    // redirect
+    location.href = "https://accounts.spotify.com/authorize?" + 
+        new URLSearchParams({
+            response_type: 'code',
+            client_id: clientId,
+            scope: scope,
+            redirect_uri: redirectUri,
+            state: state
+        }).toString();
+}
+
